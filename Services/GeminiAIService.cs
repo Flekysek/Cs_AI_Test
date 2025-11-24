b36e5e69557adf0d419c3f9bb4801bd4d156b199
Dom <fleklolik@gmail.com>
2025-11-24 20:20:23 +0100

    Just renaming

--- Services/GeminiAIService.cs
+++ Services/GeminiAIService.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Configuration; 
 using Google.Cloud.AIPlatform.V1; 
-using Google.Protobuf.WellKnownTypes; 
+using Google.Protobuf.WellKnownTypes;
+using Google.Apis.Auth.OAuth2; 
+using Grpc.Auth; 
+using System.IO; 
 
 namespace FoodAI.Services
 {
@@ -24,9 +27,27 @@ namespace FoodAI.Services
             _publisher = configuration.GetValue<string>("GeminiAI:Publisher");
             _model = configuration.GetValue<string>("GeminiAI:Model");
 
+            GoogleCredential credential = null;
+            var googleCredentialsJson = Environment.GetEnvironmentVariable("GOOGLE_CREDENTIALS_JSON");
+
+            if (!string.IsNullOrEmpty(googleCredentialsJson))
+            {
+                Console.WriteLine("Found GOOGLE_CREDENTIALS_JSON environment variable. Attempting to load credentials from JSON string.");
+                credential = GoogleCredential.FromJson(googleCredentialsJson)
+                                             .CreateScoped(PredictionServiceClient.DefaultScopes);
+            }
+            else
+            {
+                Console.WriteLine("GOOGLE_CREDENTIALS_JSON environment variable not set. Attempting default credentials.");
+                // Fallback to default credential discovery
+                credential = GoogleCredential.GetApplicationDefault()
+                                             .CreateScoped(PredictionServiceClient.DefaultScopes);
+            }
+
             _predictionServiceClient = new PredictionServiceClientBuilder
             {
-                Endpoint = $"{_location}-aiplatform.googleapis.com"
+                Endpoint = $"{_location}-aiplatform.googleapis.com",
+                ChannelCredentials = credential.ToChannelCredentials() // Explicitly set credentials
             }.Build();
         }
 
